95b7a4dbe9b277d7db8c4d69b8b7da8e8c8caeec
May Phoo Wint Zay Oo <mayphoowintzayoo@gmail.com>
2021-07-18 00:11:07 +0630

    Update Project Section

--- Controllers/ProjectController.cs
+++ Controllers/ProjectController.cs
@@ -6,22 +6,77 @@ using System.Web.Mvc;
 using Profolio_ASPDotNet.Models;
 using Profolio_ASPDotNet.Models.ProjectsModel;
 
+
 namespace Profolio_ASPDotNet.Controllers
 {
     public class ProjectController : Controller
     {
         // GET: Project
-        public ActionResult ViewProject()
+        public ActionResult ViewProgrammingLanguage()
         {
             var project = new List<Project> {
-                new Project {PLanguage="C#"},
-                new Project {PLanguage = "Python"}
+                new Project {Id=0, ProjectName="Profolio",
+                    ProjectLink="https://github.com/MayPhoo-tech-2020/Profolio-ASP.Net-MVC",
+                    ProjectDescription="Hello World",PLanguage="C#"},
+                new Project {Id=1, ProjectName="WebScrappingDota2Data",
+                    ProjectLink="https://github.com/MayPhoo-tech-2020/WebScrappingDota2Data",
+                    ProjectDescription="Hello World",PLanguage="Python"},
+                new Project {Id=2, ProjectName="Profolio",
+                    ProjectLink="https://github.com/MayPhoo-tech-2020/AirBNB-ASP.NET-MVC/tree/main/AirBnB/Views/Customer",
+                    ProjectDescription="Hello World",PLanguage="C#"},
+                new Project {Id=3, ProjectName="WebScrappingDota2Data",
+                    ProjectLink="https://github.com/MayPhoo-tech-2020/ETLDevelopment-WorldBankData",
+                    ProjectDescription="Hello World",PLanguage="Python"}
             };
             var projectViewModel = new ProjectsViewModel
             {
                 Projects = project
-        };
+            };
             return View(projectViewModel);
+        }
+
+        public ActionResult ViewProject(string pLanguage)
+		{
+            var project = new List<Project> {
+                new Project {Id=0, ProjectName="Profolio",
+                    ProjectLink="https://github.com/MayPhoo-tech-2020/Profolio-ASP.Net-MVC",
+                    ProjectDescription="Hello World",PLanguage="C#"},
+                new Project {Id=1, ProjectName="WebScrappingDota2Data",
+                    ProjectLink="https://github.com/MayPhoo-tech-2020/WebScrappingDota2Data",
+                    ProjectDescription="Hello World",PLanguage="Python"},
+                new Project {Id=2, ProjectName="AirBNB-ASP.NET",
+                    ProjectLink="https://github.com/MayPhoo-tech-2020/AirBNB-ASP.NET-MVC/tree/main/AirBnB/Views/Customer",
+                    ProjectDescription="Hello World",PLanguage="C#"},
+                new Project {Id=3, ProjectName="ETLDevelopment-WorldBankData",
+                    ProjectLink="https://github.com/MayPhoo-tech-2020/ETLDevelopment-WorldBankData",
+                    ProjectDescription="Hello World",PLanguage="Python"}
+            };
+            
+            var projectViewModel = new ProjectsViewModel
+            {
+                Projects = project.FindAll(x => x.PLanguage == pLanguage)
+            };
+            Console.WriteLine(projectViewModel);
+            return View(projectViewModel);
+		}
+        public ActionResult ViewProjectDetails(int id)
+        {
+            var project = new List<Project> {
+                new Project {Id=0, ProjectName="Profolio",
+                    ProjectLink="https://github.com/MayPhoo-tech-2020/Profolio-ASP.Net-MVC",
+                    ProjectDescription="Hello World",PLanguage="C#"},
+                new Project {Id=1, ProjectName="WebScrappingDota2Data",
+                    ProjectLink="https://github.com/MayPhoo-tech-2020/WebScrappingDota2Data",
+                    ProjectDescription="Hello World",PLanguage="Python"},
+                new Project {Id=2, ProjectName="Profolio",
+                    ProjectLink="https://github.com/MayPhoo-tech-2020/AirBNB-ASP.NET-MVC/tree/main/AirBnB/Views/Customer",
+                    ProjectDescription="Hello World",PLanguage="C#"},
+                new Project {Id=3, ProjectName="WebScrappingDota2Data",
+                    ProjectLink="https://github.com/MayPhoo-tech-2020/ETLDevelopment-WorldBankData",
+                    ProjectDescription="Hello World",PLanguage="Python"}
+            };
+            var projectDetail = project.Find(x => x.Id == id);
+            return View(projectDetail);
         }
     }
 }
